d80120e69a19eb18cf0236229e653e647a38b5d2
Haad Khan <haad@cerebri.com>
2016-07-25 17:33:38 -0500

    bare minimum code to run

--- hello.py
+++ hello.py
@@ -1,17 +1,14 @@
-
-"""Cloud Foundry test"""
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
-# On Bluemix, get the port number from the environment variable VCAP_APP_PORT
-# When running this app on the local machine, default the port to 8080
-port = int(os.getenv('VCAP_APP_PORT', 8080))
-
 @app.route('/')
-def hello_world():
-    return 'Hello World! I am running on port ' + str(port)
+def Hello():
+	return 'This is test'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+#port = os.getenv('PORT', '8080')
+port = int(os.getenv('VCAP_APP_PORT', '5000'))
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0',port=port)
