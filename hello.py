a222b72b5455a283be2721ac88c84b262aefdf16
Haad Khan <haad@cerebri.com>
2016-07-26 15:09:49 -0500

    implemented pi integration for mock data

--- hello.py
+++ hello.py
@@ -1,14 +1,38 @@
 import os
 from flask import Flask
+from personality_insights_wrapper import PersonalityInsightCaller
 
 app = Flask(__name__)
 
 @app.route('/')
 def Hello():
-	return 'This is test'
+    return 'This is mere test. Try the /pitest route'
+
+@app.route('/pi')
+def PIroute():
+    username = os.getenv('PIUsername', 'MOCK')
+    password = os.getenv('PIPassword','MOCK')
+    url = os.getenv('PIUrl','MOCK')
+    creds = {'username':username,'password':password,'url':url}
+    PIDemo = PersonalityInsightCaller(creds)
+    PIDemo.insert_text('Random text for the mock code.')
+    insights = PIDemo.get_personality()    
+    return insights
+
+@app.route('/pitest')
+def PIroute():
+    username = 'MOCK'
+    password = 'MOCK'
+    url = 'MOCK'
+    creds = {'username':username,'password':password,'url':url}
+    PIDemo = PersonalityInsightCaller(creds)
+    PIDemo.insert_text('Random text for the mock code.')
+    insights = PIDemo.get_personality()
+    return insights
+
 
 #port = os.getenv('PORT', '8080')
 port = int(os.getenv('VCAP_APP_PORT', '5000'))
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0',port=port)
+    app.run(host='0.0.0.0',port=port)
