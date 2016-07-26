f07766520fd6ddcdae4a7996a12ece3421b41939
Haad Khan <haad@cerebri.com>
2016-07-26 10:29:54 -0500

    the .sh file to source before locally running.

---
+++ env_var_example.sh
@@ -0,0 +1,7 @@
+export BluemixAppName=""
+export BluemixHostName=""
+export USER=""@cerebri.com
+export PASSWORD=""
+export url=https://gateway.watsonplatform.net/personality-insights/api
+export PIPassword=bluemixpassword
+export PIUsername=bluemixusername
