b06524e11287ee02cbdd68d14c2a32adb0ba2cb8
Haad Khan <haad@cerebri.com>
2016-07-29 13:28:10 -0500

    Adding testing and linting. Also linted the code

--- server.py
+++ server.py
@@ -2,21 +2,22 @@ import os
 from flask import Flask
 from flask import request
 from flask import json
-from flask import jsonify
 from watsoncaller.personality_insights_wrapper import PersonalityInsight
 
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return 'Server is working try the /pitest route'
 
+
 @app.route('/test')
 def hello():
     return 'Hello, World'
 
-@app.route('/piroute', methods=['GET','POST'])
+
+@app.route('/piroute', methods=['GET', 'POST'])
 def PIroute():
     if request.method == 'GET':
         return "Server is running and route is active"
@@ -24,14 +25,16 @@ def PIroute():
         if request.headers['Content-Type'] == 'application/json':
             validated_json = json_validation(request.json)
             return str(pi_instantiation().return_pi(validated_json))
-        else :
+        else:
             return "Invalid data"
+
 
 @app.route('/pitest')
 def PItest():
     return str(pi_instantiation(mock=True).return_pi('mock data'))
 
 port = int(os.getenv('VCAP_APP_PORT', '5000'))
+
 
 def json_validation(json_data):
     try:
@@ -41,17 +44,20 @@ def json_validation(json_data):
     except:
         raise Exception("Bad Data")
 
-def pi_instantiation(mock = False):
+
+def pi_instantiation(mock=False):
     if mock:
-        return PersonalityInsight({'MOCK':True, 'username': True,'password': True, 'url': True})
+        return PersonalityInsight({'MOCK': True, 'username': True,
+                                   'password': True, 'url': True})
     else:
         try:
             username = os.getenv('PIUsername')
             password = os.getenv('PIPassword')
             url = os.getenv('PIUrl')
-            return PersonalityInsight({'username':username,'password':password,'url':url})
+            return PersonalityInsight({'username': username,
+                                       'password': password, 'url': url})
         except:
-            raise Exception("environment variables dont exist")   
+            raise Exception("environment variables dont exist")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=port)
+    app.run(host='0.0.0.0', port=port)
