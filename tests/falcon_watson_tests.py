702b8ef1e0f57811d874936ae39f76351e699649
Jesse Macht <jesse@cerebri.com>
2016-08-03 09:14:05 -0400

    adding logical error so tests fail to see if circleCI is working properly

--- tests/falcon_watson_tests.py
+++ tests/falcon_watson_tests.py
@@ -1,11 +1,8 @@
 from nose.tools import *
 import watson_connector
 
-def setup():
-	print "SETUP!"
-
-def teardown():
-	print "TEAR DOWN!"
-
 def test_basic():
 	print "I RAN!"
+
+def test_confuse():
+	assert 1 > 2
