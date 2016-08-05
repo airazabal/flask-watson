d56d1c0c88dcd6a9c72c9a6e95cbba8d5d1a0e60
Jesse Macht <jesse@cerebri.com>
2016-08-05 09:15:13 -0400

    slight tweaks, comments

--- tests/test_fb_oauth_selenium_driver.py
+++ tests/test_fb_oauth_selenium_driver.py
@@ -13,19 +13,21 @@ class OauthScript(object):
 		self.fbPassword = str(os.getenv('FB_OAUTH_PASSWORD', 'MarkyMark2016')) 
 
 	def setup(self):
-		#open the mini-oauth server that will ask the user if they authorize the app and turn off security settings
+		#open the flask-front-end bluemix server
 		self.server = webdriver.Firefox()
-
-		self.server.get('http://localhost:5001/oauth')
+		self.server.get('http://flask-front-end.mybluemix.net')
 		assert "Cerebri Oauth Tester" in self.server.title
 
 	def run(self):
+		#click the 'Authorize!' button
 		auth = self.server.find_element(By.NAME, "authorize")
 		auth.click()
 
+		#switch to the popup window that asks for a Facebook login
 		self.server.switch_to_window(self.server.window_handles[1])
 		assert "Facebook" in self.server.title
 
+		#enter Facebook credentials and hit the login button
 		loginEmail = self.server.find_element(By.NAME, "email")
 		loginEmail.send_keys(self.fbEmail)
 		loginPass = self.server.find_element(By.NAME, "pass")
@@ -33,12 +35,16 @@ class OauthScript(object):
 		loginSubmit = self.server.find_element(By.NAME, "login")
 		loginSubmit.click()
 
+		#checks to see if the window has closed - if the user is new to the app and hasn't authenticated before
+		#it will ask them to confirm the set of permissions that Facebook is asking for
 		if len(self.server.window_handles) == 2:
 			confirm = self.server.find_element(By.NAME, "__CONFIRM__")
 			confirm.click()
 
+		#switch back to the main window
 		self.server.switch_to_window(self.server.window_handles[0])
 
+		#check the text indicator as to how the POST request to the watson connector went
 		checker = self.server.find_element(By.ID, "auth_check")
 		assert checker.text == "SUCCESS"
 
