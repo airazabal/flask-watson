4319c6baada13f9a29beec3e5c44a3c35072b928
Spencer Romo <spencer@cerebri.com>
2016-08-08 10:45:22 -0500

    Changing the credentials.  Adding in a delay.

--- tests/test_fb_oauth_selenium_driver.py
+++ tests/test_fb_oauth_selenium_driver.py
@@ -3,14 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from nose.tools import *
+import time 
+
 
 class OauthScript(object):
 	"""A test Oauth user sign in for Facebook access"""
 
 	def __init__(self):
 		#test user from Cerebri AI test Facebook App
-		self.fbEmail = str(os.getenv('FB_OAUTH_EMAIL','xmcmlmu_thurnson_1469652579@tfbnw.net'))
-		self.fbPassword = str(os.getenv('FB_OAUTH_PASSWORD', 'MarkyMark2016')) 
+		self.fbEmail = str(os.getenv('FB_OAUTH_EMAIL','spencer@cerebri.com'))
+		self.fbPassword = str(os.getenv('FB_OAUTH_PASSWORD', 'SarahBri')) 
 
 	def setup(self):
 		#open the flask-front-end bluemix server
@@ -37,7 +39,7 @@ class OauthScript(object):
 
 		#switch back to the main window
 		self.server.switch_to_window(self.server.window_handles[0])
-
+		time.sleep(5)
 		#check the text indicator as to how the POST request to the watson connector went
 		checker = self.server.find_element(By.ID, "auth_check")
 		print checker.text, 'Checker text'
