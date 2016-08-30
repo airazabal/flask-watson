2f5fe3b2b474c2b2b5098f58dac8585aa31465b2
Spencer Romo <spencer@cerebri.com>
2016-08-30 17:16:38 -0500

    adding in tests for google plus stuffs.

--- tests/test_fb_oauth_selenium_driver.py
+++ tests/test_fb_oauth_selenium_driver.py
@@ -13,14 +13,19 @@ class OauthScript(object):
 		#test user from Cerebri AI test Facebook App
 		self.fbEmail = str(os.getenv('FB_OAUTH_EMAIL'))
 		self.fbPassword = str(os.getenv('FB_OAUTH_PASSWORD')) 
+		self.gEmail = str(os.getenv('G_EMAIL'))
+		self.gPW = str(os.getenv('G_PW'))
 
-	def setup(self):
+	def setupFB(self):
 		#open the flask-front-end bluemix server
 		self.server = webdriver.PhantomJS()
 		self.server.get('http://flask-front-end.mybluemix.net/oauth')
 		assert "Cerebri Oauth Tester" in self.server.title
+		
+	def setupG(self):
+		self.server.get('http://flask-front-end.mybluemix.net/google')
 
-	def run(self):
+	def runFB(self):
 		#click the 'Authorize!' button
 		auth = self.server.find_element(By.NAME, "authorize")
 		auth.click()
@@ -45,11 +50,41 @@ class OauthScript(object):
 		print checker.text, 'Checker text'
 		assert checker.text == "SUCCESS"
 
+	def runG(self):
+		#click the 'Authorize!' button
+		auth = self.server.find_element(By.ID, "gConnect")
+		auth.click()
+
+		#switch to the popup window that asks for a Facebook login
+		self.server.switch_to_window(self.server.window_handles[1])
+		assert "Sign in - Google Accounts" in self.server.title
+
+		#enter Facebook credentials and hit the login button
+		loginEmail = self.server.find_element(By.NAME, "Email")
+		loginEmail.send_keys(self.gEmail)
+		clickCont = self.server.find_element(By.NAME, 'signIn')
+		clickCont.click()
+
+		loginPass = self.server.find_element(By.NAME, "Passwd")
+		loginPass.send_keys(self.gPW)
+		loginSubmit = self.server.find_element(By.NAME, "signIn")
+		loginSubmit.click()
+
+		#switch back to the main window
+		self.server.switch_to_window(self.server.window_handles[0])
+		time.sleep(5)
+		#check the text indicator as to how the POST request to the watson connector went
+		checker = self.server.find_element(By.ID, "status")
+		print checker.text, 'Checker text'
+		assert checker.text == "SUCCESS"
+
 	def teardown(self):
 		self.server.close()
 
 def test_ouath_flow():
 	main = OauthScript()
-	main.setup()
-	main.run()
+	main.setupFB()
+	main.runFB()
+	main.setupG()
+	main.runG()
 	main.teardown()
